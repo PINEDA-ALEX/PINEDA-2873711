b8330577642c0f3370107fbfe9fa95ebaf8e8753
alexpineda <pinedaosoriojhonalexander@gmail.com>
2024-04-25 11:02:33 -0500

    sisi

---
+++ ejerciciosGUIA3/condicionales Dobles/condicionalDoble02.dart
@@ -0,0 +1,13 @@
+import'dart:io'
+void main(){
+
+
+/*Hacer un algoritmo que calcule el total a pagar por la compra de 
+camisas. Si se compran tres camisas o mas se aplica un descuento del 20%  sobre el total de la 
+compra y si son menos de tres camisas un descuento del 10% */
+//definir las variables
+int camisas;
+double precio,
+//
+
+}
