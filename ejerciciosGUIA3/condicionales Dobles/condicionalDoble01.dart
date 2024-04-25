01d596f60734348afd24d0adc1e62920ef843ac3
alexpineda <pinedaosoriojhonalexander@gmail.com>
2024-04-25 11:58:33 -0500

    trabajo

--- ejerciciosGUIA3/condicionales Dobles/condicionalDoble01.dart
+++ ejerciciosGUIA3/condicionales Dobles/condicionalDoble01.dart
@@ -1,15 +1,17 @@
-void main( lis>String> arfs){
+import 'dart:io';
+
+void main( List<String> arfs){
     // DEFINICION DE VARIABLES 
     double num1, num2;
     //entrada Alg 
     print( "ingrese numeros 1 y 2");
-    num1 = double.parse(stdin.readlineSync()!);
-    num2 = double.parse(stdin.readlineSync()!);
+    num1 = double.parse(stdin.readLineSync()!);
+    num2 = double.parse(stdin.readLineSync()!);
     //PRECESO SALIDA
-    is (num1 > num2 ){
+    if(num1 > num2){
         print("$num2, $num1");
 
-    }else {
+    }else{
         print ("$num1,$num2");
     }
 
--- ejerciciosGUIA3/condicionales Dobles/condicionalDoble02.dart
+++ ejerciciosGUIA3/condicionales Dobles/condicionalDoble02.dart
@@ -1,4 +1,5 @@
-import'dart:io'
+import 'dart:io';
+
 void main(){
 
 
@@ -6,8 +7,25 @@ void main(){
 camisas. Si se compran tres camisas o mas se aplica un descuento del 20%  sobre el total de la 
 compra y si son menos de tres camisas un descuento del 10% */
 //definir las variables
-int camisas;
-double precio,
-//
+
+
+//DEFINICION DE VARIABLES 
+int cantidadCamisas ;
+double precioCamisa, totaldeCamisas;
+
+cantidadCamisas = 0;
+//ENTRADA DE DATOS
+print("INGRESE LA CANTODAD DE CAMIASAS QUE SE VAN A COMPRAR");
+cantidadCamisas = int.parse(stdin.readLineSync()!);
+print("ingrese el precio de cada camisa");
+precioCamisa = double.parse(stdin.readLineSync()!);
+//proceso y salida
+totaldeCamisas = cantidadCamisas*precioCamisa;
+print("la cantidad de a pagar es de: $totaldeCamisas");
+
+
+
+
+
 
 }
