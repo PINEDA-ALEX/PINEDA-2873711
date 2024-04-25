9ed43249cf31b39ddafae0e7ada6703d283550db
alexpineda <pinedaosoriojhonalexander@gmail.com>
2024-04-25 10:13:58 -0500

    hola

---
+++ ejerciciosGUIA3/condicionales Dobles/condicionalSimple01.dart
@@ -0,0 +1,16 @@
+void main( lis>String> arfs){
+    // DEFINICION DE VARIABLES 
+    double num1, num2;
+    //entrada Alg 
+    print( "ingrese numeros 1 y 2");
+    num1 = double.parse(stdin.readlineSync()!);
+    num2 = double.parse(stdin.readlineSync()!);
+    //PRECESO SALIDA
+    is (num1 > num2 ){
+        print("$num2, $num1");
+
+    }else {
+        print ("$num1,$num2");
+    }
+
+}
